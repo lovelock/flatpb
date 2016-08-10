60f3a428571632be44700435f463fe35c9aace8b
lovelock <frostwong@gmail.com>
2016-08-10 11:39:59 +0800

    skeleton

---
+++ src/Http/Client.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 8/8/16
+ * Time: 9:04 PM
+ */
+
+namespace Http;
+
+
+class Client
+{
+
+}
---
+++ src/Http/Request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 8/8/16
+ * Time: 9:05 PM
+ */
+
+namespace Http;
+
+
+class Request
+{
+
+}
